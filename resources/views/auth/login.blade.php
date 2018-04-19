55456445c8932d9ebfbcd59b281d99d676985448
noksit <noksit@live.fr>
2018-04-19 10:34:22 +0200

    ptite modif de login.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,12 +44,8 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Connexion') }}
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
                             </div>
                         </div>
                     </form>
