fd6b96419460ce274b46f0532e15085bc1dac24c
noksit <noksit@live.fr>
2018-05-17 10:05:11 +0200

    ajout bnt 'voir tous les tutos' en index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,14 @@
 
                 <!-- Entete de la catégorie-->
                 <div class="col-md-12 m-5">
-                    <h5><b>Derniers tutoriels de nos formateurs:</b></h5>
+                    <div class="row">
+                        <div class="col-6">
+                            <h5><b>Derniers tutoriels de nos formateurs:</b></h5>
+                        </div>
+                        <div class="col-6 text-right pr-5">
+                            <a href="{{route('listing-all')}}" class="btn btn-light"><h5>Voir tous les tutoriels</h5></a>
+                        </div>
+                    </div>
                 </div>
                 <div class="row justify-content-center">
                     @include('components.Publication.tutoriel')
@@ -31,15 +38,16 @@
                 <div class="mb-3">
                     <h6 class="m-3 text-secondary border-bottom"><b>Derniers Membres</b></h6>
                     <div class="row">
-                    @foreach($users as $user)
-                        <div class="row justify-content-center m-2">
+                        @foreach($users as $user)
+                            <div class="row justify-content-center m-2">
                                 <a class="btn btn-light w50" href="{{route('other-profil',['slug' => $user->slug])}}">
-                                    <img class=" card-img-top img-fluid rounded-circle" src="{{asset($user->imgprofil)}}"
+                                    <img class=" card-img-top img-fluid rounded-circle"
+                                         src="{{asset($user->imgprofil)}}"
                                          alt="Image de profil"><br>
                                     {{ucfirst(strtolower($user->firstname))}}
                                 </a>
-                        </div>
-                    @endforeach
+                            </div>
+                        @endforeach
                     </div>
                 </div>
                 <div class="border border-light rounded">
