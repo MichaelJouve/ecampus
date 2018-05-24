98375a4071487cbb923bbf6e988ce838d6cb300a
Tony07200 <baptiste.anthony07@gmail.com>
2018-05-24 13:42:50 +0200

    Restructuration de la page Profil

--- resources/views/components/Publication/bestTutorial.blade.php
+++ resources/views/components/Publication/bestTutorial.blade.php
@@ -1,9 +1,9 @@
 
 
-    <div class="col-md-12 bandeau-sombre p-4 rounded">
+    <div class="col-md-12 bg-light text-secondary shadow p-4 rounded">
         <div class="row">
             <div class="col-md-8">
-                <h2>
+                <h2 class="font-weight-bold">
                     {{ $bestTutorial->title }}
                 </h2>
                 <p>
@@ -18,12 +18,12 @@
             </span>
                 <p class="small">
                     Proposé par :
-                    <a class="link_bandeau"
+                    <a class="link_bandeau text-secondary"
                        href="{{route('other-profil',['slug' => $bestTutorial->user->slug])}}">{{ $bestTutorial->user->name }} {{ $bestTutorial->user->firstname }}</a>
                 </p>
 
                 <a href="{{route('front-tutorial',['slug' => $bestTutorial->slug])}}">
-                    <button class="bg-light border-0 rounded mt-3 ml-1 p-2">Découvrir le tutoriel</button>
+                    <button class="bg-info text-light border-0 rounded mt-3 ml-1 p-2">Découvrir le tutoriel</button>
                 </a>
             </div>
 
