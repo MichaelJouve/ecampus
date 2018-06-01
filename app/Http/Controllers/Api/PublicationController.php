f0c7987f98f78f090d4805337ed45222a3ac6838
Raziel-777 <thibault.dams@gmail.com>
2018-06-01 15:50:10 +0200

    test

--- app/Http/Controllers/Api/PublicationController.php
+++ app/Http/Controllers/Api/PublicationController.php
@@ -33,7 +33,7 @@ class PublicationController extends Controller
      */
     public function show($id)
     {
-        response()->json(Publication::find($id)->with('category', 'user', 'consultation', 'comment', 'userOwner'));
+        return response()->json(Publication::find($id)->with('category', 'user', 'consultation', 'comment', 'userOwner'));
     }
 
     /**
