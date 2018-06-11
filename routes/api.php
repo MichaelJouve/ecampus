e3e24eee7cb9b268a78aedc87dcff6b0cb82232c
Raziel-777 <thibault.dams@gmail.com>
2018-06-11 10:14:44 +0200

    api user register

--- routes/api.php
+++ routes/api.php
@@ -49,4 +49,4 @@ Route::middleware('auth:api')->group(function () {
     });
 });
 
-    Route::post('register', 'Auth\RegisterController@register');
+    Route::post('register', 'Auth\RegisterController@registerApi');
